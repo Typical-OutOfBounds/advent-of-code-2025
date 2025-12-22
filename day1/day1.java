ec5258263dabbedbe81af8357de6e78f0a860894
Aidan Lane <aklane.work@gmail.com>
2025-12-22 15:53:16 -0500

    add file read function to simplify code

--- day1/day1.java
+++ day1/day1.java
@@ -9,23 +9,14 @@ public class Day1 {
         String part = args[0];
         if (part.equals("part1")) {
             part1(args);
+        } else if (part.equals("part2")) {
+            part2(args);
         }
     }
 
     public static void part1(String[] args) {
         String filename = args[1];
-        File myFile = new File(filename);
-        List<String> commands = new ArrayList<>();
-        
-        try (Scanner myReader = new Scanner(myFile)) {
-            while (myReader.hasNextLine()) {
-                commands.add(myReader.nextLine().strip());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Not able to find file");
-            e.printStackTrace();
-        }
-        System.out.println(commands);
+        List<String> commands = readCommands(filename);
 
         int pos = 50;
         int zeroCount = 0;
@@ -39,6 +30,21 @@ public class Day1 {
         }
         
         System.out.println(zeroCount);
+    }
+
+    public static List<String> readCommands(String filename) {
+        File myFile = new File(filename);
+        List<String> commands = new ArrayList<>();
+        
+        try (Scanner myReader = new Scanner(myFile)) {
+            while (myReader.hasNextLine()) {
+                commands.add(myReader.nextLine().strip());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Not able to find file");
+            e.printStackTrace();
+        }
+        return commands;
     }
 
     public static int rotate(int pos, String direction, int rotationAmount) {
@@ -57,5 +63,23 @@ public class Day1 {
             }
         }
         return pos;
+    }
+
+    public static void part2(String[] args) {
+        String filename = args[1];
+        List<String> commands = readCommands(filename);
+
+        int pos = 50;
+        int zeroCount = 0;
+        for (String command : commands) {
+            String direction = command.substring(0,1);
+            int rotationAmount = Integer.parseInt(command.substring(1));
+            pos = rotate(pos, direction, rotationAmount);
+            if (pos == 0) {
+                zeroCount = zeroCount + 1;
+            }
+        }
+        
+        System.out.println(zeroCount);
     }
 }
